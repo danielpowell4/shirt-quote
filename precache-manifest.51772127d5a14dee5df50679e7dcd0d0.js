21690c60acdbca625f9b315cf6fabf5a827e32b2
Dan <danielpowell12@msn.com>
2019-09-01 17:37:37 -0600

    Updates

---
+++ precache-manifest.51772127d5a14dee5df50679e7dcd0d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8416da746d4852665dccabe9eee889d6",
+    "url": "/shirt-quote/index.html"
+  },
+  {
+    "revision": "441d2d98647a7decb868",
+    "url": "/shirt-quote/static/css/main.ce65717b.chunk.css"
+  },
+  {
+    "revision": "8ccb48c4f8a8e16f32f9",
+    "url": "/shirt-quote/static/js/2.5dfdc5d5.chunk.js"
+  },
+  {
+    "revision": "441d2d98647a7decb868",
+    "url": "/shirt-quote/static/js/main.e4e475a5.chunk.js"
+  },
+  {
+    "revision": "24b25ea7cedf7dee0f8a",
+    "url": "/shirt-quote/static/js/runtime~main.1cc5dde5.js"
+  }
+]);
